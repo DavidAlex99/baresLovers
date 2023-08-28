c523189f4f6c63ba63822b81b8824ce5a42928e2
kenethPs <68650722+kenethPs@users.noreply.github.com>
2023-08-27 23:39:10 -0500

    actualizacion de la vista nuevo post

--- baresLovers/src/app/app-routing.module.ts
+++ baresLovers/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GridBarComponent } from './components/grid-bar/grid-bar.component';
 import { DetallesBarComponent } from './components/detalles-bar/detalles-bar.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
 const routes: Routes = [
   { path: '', component: GridBarComponent },
-  { path: 'bares/:barId', component: DetallesBarComponent }
+  { path: 'bares/:barId', component: DetallesBarComponent },
+  { path: '', component: NewPostComponent}
 ];
 
 @NgModule({
