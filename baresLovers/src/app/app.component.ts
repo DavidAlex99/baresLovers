c1eb85aca1663e9a303f618f08e12f68b6af151d
DavidAlex99 <daalrive@espol.edu.ec>
2023-08-27 21:12:07 -0500

    arreglo parcia; visualizacion bar

--- baresLovers/src/app/app.component.ts
+++ baresLovers/src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 
+//Importación de la interfaz
+import {Root2} from './interfaces/informacion-bar';
+
+ //Importación del servicio
+import {MongodbAPIService} from './services/barPrincipal/main-bar.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'baresLovers';
+  title = 'baresLovers';
 
-  // //Inyección de dependencia del servicio
-  // constructor(private dataProvider: MongodbAPIService) { }
+  //Inyección de dependencia del servicio
+  constructor(private dataProvider: MongodbAPIService) { }
 
-  // //Atributo con el tipo de dato de la interfaz
-  // public data : Root2[] = [];
+  //Atributo con el tipo de dato de la interfaz
+  public data : Root2[] = [];
 
-  // //Ejecución de la petición y suscripción de la respuesta
-  // ngOnInit() {
-  //   this.dataProvider.getResponse().subscribe((response) => { 
-  //     this.data = (response as Root2[]); 
-  //   })
-  // } 
+  //Ejecución de la petición y suscripción de la respuesta
+  ngOnInit() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      this.data = (response as Root2[]); 
+    })
+  } 
 }
